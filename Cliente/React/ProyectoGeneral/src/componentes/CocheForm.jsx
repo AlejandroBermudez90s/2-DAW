4ecaebd5d48d71a9af7936e2010a814eb7a76001
Alejandro Bermúdez <2060559@alu.murciaeduca.es>
2026-01-13 12:25:24 +0100

    React @mui library

---
+++ Cliente/React/ProyectoGeneral/src/componentes/CocheForm.jsx
@@ -0,0 +1,146 @@
+import { useForm } from 'react-hook-form';
+import TextField from '@mui/material/TextField'
+import '@fontsource/roboto/300.css'
+import NumberSpinner from './NumberSpinner';
+
+const CocheForm = (props) => {
+
+  const COCHE = {
+
+    // Identificadores de los campos del formulario, se los asignaremos al atributo id
+    MODELO: "modelo",
+    MATRICULA: "matricula",
+    COLOR: "color",
+    PUERTAS: "npuertas",
+  }
+
+  // Objeto coche en su estado inicial
+  const COCHEINICIAL = {
+    modelo: "",
+    matricula: "",
+    color: "",
+    npuertas: 2
+  }
+
+  const { register,
+    handleSubmit,
+    reset,
+    setValue,
+    formState: { errors },
+    watch } = useForm({ defaultValues: COCHEINICIAL });
+
+  console.log(errors);
+
+  // Función encargada de actualiar el hook de estado del componente y
+  // devolver el coche creado al objeto padre
+  const manejarFormulario = handleSubmit((nuevoCoche) => {
+
+    console.log(nuevoCoche);
+
+    // A través de la función que recibe el componente como props, devolvemos el nuevo coche
+    // al componente padre para que lo añada a la lista de coches
+    props.manejarAccion(nuevoCoche);
+
+    reset(COCHEINICIAL);
+  })
+
+  return (
+    <form id="formulario" onSubmit={manejarFormulario}>
+      <p><strong>Nuevo vehículo</strong></p>
+      <TextField id={COCHE.MODELO} label="Modelo" variant="outlined"
+        {...register(COCHE.MODELO,
+          {
+            required: {
+              value: true,
+              message: "El modelo es obligatorio"
+            }
+          })
+        }
+      /><br></br><span>{errors.modelo?.message}</span><br></br>
+
+
+      <TextField id={COCHE.MATRICULA} label="Matrícula" variant="outlined"
+        {...register(COCHE.MATRICULA,
+          {
+            required: {
+              value: true,
+              message: "La matrícula es obligatoria"
+            }
+            ,
+            pattern: {
+              value: /^\d\d\d\d[BCDFGHJKLMNPRSTVWXYZ][BCDFGHJKLMNPRSTVWXYZ][BCDFGHJKLMNPRSTVWXYZ]$/,
+              message: "El formato de la matrícula no es correcto 0000AAA"
+            }
+          })
+        }
+      /><br></br><span>{errors.matricula?.message}</span><br></br>
+
+      <TextField id={COCHE.COLOR} label="Color" variant="filled"
+        {...register(COCHE.COLOR,
+          {
+            required: {
+              value: true,
+              message: "El color es obligatorio"
+            }
+          })
+        }
+      /><br></br><span>{errors.color?.message}</span><br></br>
+
+      <NumberSpinner id = {COCHE.PUERTAS}
+                     label = "Nº Puertas"
+                     min = {2}
+                     max = {5}
+                     size = "small"
+                     defaultValue = {2}
+                     onValueChange = {(value) => {
+                                                    setValue(COCHE.PUERTAS,
+                                                             value, {shouldDirty    : true,
+                                                                     shouldValidate : true 
+                                                             } 
+                                                    )
+                                                  }
+                                      }  
+        {...register(COCHE.PUERTAS,
+          {
+            required: {
+              value: true,
+              message: "El número de puertas es obligatorio"
+            },
+            min: {
+              value: 2,
+              message: "Valor mínimo para este campo es 2"
+            },
+            max: {
+              value: 5,
+              message: "Valor máximo para este campo es 5"
+            }
+          })
+        } /><br></br><span>{errors.npuertas?.message}</span><br></br>
+
+
+      {/*
+      <TextField id={COCHE.PUERTAS} label="Nº Puertas" variant="standard"
+        {...register(COCHE.PUERTAS,
+          {
+            required: {
+              value: true,
+              message: "El número de puertas es obligatorio"
+            },
+            min: {
+              value: 2,
+              message: "Valor mínimo para este campo es 2"
+            },
+            max: {
+              value: 5,
+              message: "Valor máximo para este campo es 5"
+            }
+          })
+        } /><br></br><span>{errors.npuertas?.message}</span><br></br>
+*/}
+      <button>Añadir Coche</button>
+      <br></br>{JSON.stringify(watch())}
+    </form>
+  )
+}
+
+export default CocheForm;
