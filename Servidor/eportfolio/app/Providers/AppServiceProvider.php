b91d8b153af0d15ceb82a6f42143aa0b807a3966
Alejandro Bermúdez <2060559@alu.murciaeduca.es>
2026-02-11 12:08:24 +0100

    tests servidor

--- Servidor/eportfolio/app/Providers/AppServiceProvider.php
+++ Servidor/eportfolio/app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use App\Models\CicloFormativo;
+use App\Models\User;
+use App\Policies\CicloFormativoPolicy;
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -22,6 +26,15 @@ class AppServiceProvider extends ServiceProvider
     {
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
+        });
+
+        Gate::before(function (User $user, string $ability) {
+
+            if ($user->esAdministrador($user)) {
+                return true;
+            }
+
+            return null;
         });
     }
 }
